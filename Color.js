eb1e64959623f41533724126cea21b85f1cb1100
jhygreatbug <jhygreatbug@gmail.com>
2018-06-08 18:56:38 +0800

    Update: keyword还有点问题

--- Color.js
+++ Color.js
@@ -9,153 +9,160 @@
 	}
 
 	var keywords = {
-		black: "000000",
-		silver: "c0c0c0",
-		gray: "808080",
-		white: "ffffff",
-		maroon: "800000",
-		red: "ff0000",
-		purple: "800080",
-		fuchsia: "ff00ff",
-		green: "008000",
-		lime: "00ff00",
-		olive: "808000",
-		yellow: "ffff00",
-		navy: "000080",
-		blue: "0000ff",
-		teal: "008080",
-		aqua: "00ffff",
-		orange: "ffa500",
-		aliceblue: "f0f8ff",
-		antiquewhite: "faebd7",
-		aquamarine: "7fffd4",
-		azure: "f0ffff",
-		beige: "f5f5dc",
-		bisque: "ffe4c4",
-		blanchedalmond: "ffe4c4",
-		blueviolet: "8a2be2",
-		brown: "a52a2a",
-		burlywood: "deb887",
-		cadetblue: "5f9ea0",
-		chartreuse: "7fff00",
-		chocolate: "d2691e",
-		coral: "ff7f50",
-		cornflowerblue: "6495ed",
-		cornsilk: "fff8dc",
-		crimson: "dc143c",
-		darkblue: "00008b",
-		darkcyan: "008b8b",
-		darkgoldenrod: "b8860b",
-		darkgray: "a9a9a9",
-		darkgreen: "006400",
-		darkgrey: "a9a9a9",
-		darkkhaki: "bdb76b",
-		darkmagenta: "8b008b",
-		darkolivegreen: "556b2f",
-		darkorange: "ff8c00",
-		darkorchid: "9932cc",
-		darkred: "8b0000",
-		darksalmon: "e9967a",
-		darkseagreen: "8fbc8f",
-		darkslateblue: "483d8b",
-		darkslategray: "2f4f4f",
-		darkslategrey: "2f4f4f",
-		darkturquoise: "00ced1",
-		darkviolet: "9400d3",
-		deeppink: "ff1493",
-		deepskyblue: "00bfff",
-		dimgray: "696969",
-		dimgrey: "696969",
-		dodgerblue: "1e90ff",
-		firebrick: "b22222",
-		floralwhite: "fffaf0",
-		forestgreen: "228b22",
-		gainsboro: "dcdcdc",
-		ghostwhite: "f8f8ff",
-		gold: "ffd700",
-		goldenrod: "daa520",
-		greenyellow: "adff2f",
-		grey: "808080",
-		honeydew: "f0fff0",
-		hotpink: "ff69b4",
-		indianred: "cd5c5c",
-		indigo: "4b0082",
-		ivory: "fffff0",
-		khaki: "f0e68c",
-		lavender: "e6e6fa",
-		lavenderblush: "fff0f5",
-		lawngreen: "7cfc00",
-		lemonchiffon: "fffacd",
-		lightblue: "add8e6",
-		lightcoral: "f08080",
-		lightcyan: "e0ffff",
-		lightgoldenrodyellow: "fafad2",
-		lightgray: "d3d3d3",
-		lightgreen: "90ee90",
-		lightgrey: "d3d3d3",
-		lightpink: "ffb6c1",
-		lightsalmon: "ffa07a",
-		lightseagreen: "20b2aa",
-		lightskyblue: "87cefa",
-		lightslategray: "778899",
-		lightslategrey: "778899",
-		lightsteelblue: "b0c4de",
-		lightyellow: "ffffe0",
-		limegreen: "32cd32",
-		linen: "faf0e6",
-		mediumaquamarine: "66cdaa",
-		mediumblue: "0000cd",
-		mediumorchid: "ba55d3",
-		mediumpurple: "9370db",
-		mediumseagreen: "3cb371",
-		mediumslateblue: "7b68ee",
-		mediumspringgreen: "00fa9a",
-		mediumturquoise: "48d1cc",
-		mediumvioletred: "c71585",
-		midnightblue: "191970",
-		mintcream: "f5fffa",
-		mistyrose: "ffe4e1",
-		moccasin: "ffe4b5",
-		navajowhite: "ffdead",
-		oldlace: "fdf5e6",
-		olivedrab: "6b8e23",
-		orangered: "ff4500",
-		orchid: "da70d6",
-		palegoldenrod: "eee8aa",
-		palegreen: "98fb98",
-		paleturquoise: "afeeee",
-		palevioletred: "db7093",
-		papayawhip: "ffefd5",
-		peachpuff: "ffdab9",
-		peru: "cd853f",
-		pink: "ffc0cb",
-		plum: "dda0dd",
-		powderblue: "b0e0e6",
-		rosybrown: "bc8f8f",
-		royalblue: "4169e1",
-		saddlebrown: "8b4513",
-		salmon: "fa8072",
-		sandybrown: "f4a460",
-		seagreen: "2e8b57",
-		seashell: "fff5ee",
-		sienna: "a0522d",
-		skyblue: "87ceeb",
-		slateblue: "6a5acd",
-		slategray: "708090",
-		slategrey: "708090",
-		snow: "fffafa",
-		springgreen: "00ff7f",
-		steelblue: "4682b4",
-		tan: "d2b48c",
-		thistle: "d8bfd8",
-		tomato: "ff6347",
-		turquoise: "40e0d0",
-		violet: "ee82ee",
-		wheat: "f5deb3",
-		whitesmoke: "f5f5f5",
-		yellowgreen: "9acd32",
-		rebeccapurple: "663399"
+		black: '000000',
+		silver: 'c0c0c0',
+		gray: '808080',
+		white: 'ffffff',
+		maroon: '800000',
+		red: 'ff0000',
+		purple: '800080',
+		fuchsia: 'ff00ff',
+		green: '008000',
+		lime: '00ff00',
+		olive: '808000',
+		yellow: 'ffff00',
+		navy: '000080',
+		blue: '0000ff',
+		teal: '008080',
+		aqua: '00ffff',
+		orange: 'ffa500',
+		aliceblue: 'f0f8ff',
+		antiquewhite: 'faebd7',
+		aquamarine: '7fffd4',
+		azure: 'f0ffff',
+		beige: 'f5f5dc',
+		bisque: 'ffe4c4',
+		blanchedalmond: 'ffe4c4',
+		blueviolet: '8a2be2',
+		brown: 'a52a2a',
+		burlywood: 'deb887',
+		cadetblue: '5f9ea0',
+		chartreuse: '7fff00',
+		chocolate: 'd2691e',
+		coral: 'ff7f50',
+		cornflowerblue: '6495ed',
+		cornsilk: 'fff8dc',
+		crimson: 'dc143c',
+		darkblue: '00008b',
+		darkcyan: '008b8b',
+		darkgoldenrod: 'b8860b',
+		darkgray: 'a9a9a9',
+		darkgreen: '006400',
+		darkgrey: 'a9a9a9',
+		darkkhaki: 'bdb76b',
+		darkmagenta: '8b008b',
+		darkolivegreen: '556b2f',
+		darkorange: 'ff8c00',
+		darkorchid: '9932cc',
+		darkred: '8b0000',
+		darksalmon: 'e9967a',
+		darkseagreen: '8fbc8f',
+		darkslateblue: '483d8b',
+		darkslategray: '2f4f4f',
+		darkslategrey: '2f4f4f',
+		darkturquoise: '00ced1',
+		darkviolet: '9400d3',
+		deeppink: 'ff1493',
+		deepskyblue: '00bfff',
+		dimgray: '696969',
+		dimgrey: '696969',
+		dodgerblue: '1e90ff',
+		firebrick: 'b22222',
+		floralwhite: 'fffaf0',
+		forestgreen: '228b22',
+		gainsboro: 'dcdcdc',
+		ghostwhite: 'f8f8ff',
+		gold: 'ffd700',
+		goldenrod: 'daa520',
+		greenyellow: 'adff2f',
+		grey: '808080',
+		honeydew: 'f0fff0',
+		hotpink: 'ff69b4',
+		indianred: 'cd5c5c',
+		indigo: '4b0082',
+		ivory: 'fffff0',
+		khaki: 'f0e68c',
+		lavender: 'e6e6fa',
+		lavenderblush: 'fff0f5',
+		lawngreen: '7cfc00',
+		lemonchiffon: 'fffacd',
+		lightblue: 'add8e6',
+		lightcoral: 'f08080',
+		lightcyan: 'e0ffff',
+		lightgoldenrodyellow: 'fafad2',
+		lightgray: 'd3d3d3',
+		lightgreen: '90ee90',
+		lightgrey: 'd3d3d3',
+		lightpink: 'ffb6c1',
+		lightsalmon: 'ffa07a',
+		lightseagreen: '20b2aa',
+		lightskyblue: '87cefa',
+		lightslategray: '778899',
+		lightslategrey: '778899',
+		lightsteelblue: 'b0c4de',
+		lightyellow: 'ffffe0',
+		limegreen: '32cd32',
+		linen: 'faf0e6',
+		mediumaquamarine: '66cdaa',
+		mediumblue: '0000cd',
+		mediumorchid: 'ba55d3',
+		mediumpurple: '9370db',
+		mediumseagreen: '3cb371',
+		mediumslateblue: '7b68ee',
+		mediumspringgreen: '00fa9a',
+		mediumturquoise: '48d1cc',
+		mediumvioletred: 'c71585',
+		midnightblue: '191970',
+		mintcream: 'f5fffa',
+		mistyrose: 'ffe4e1',
+		moccasin: 'ffe4b5',
+		navajowhite: 'ffdead',
+		oldlace: 'fdf5e6',
+		olivedrab: '6b8e23',
+		orangered: 'ff4500',
+		orchid: 'da70d6',
+		palegoldenrod: 'eee8aa',
+		palegreen: '98fb98',
+		paleturquoise: 'afeeee',
+		palevioletred: 'db7093',
+		papayawhip: 'ffefd5',
+		peachpuff: 'ffdab9',
+		peru: 'cd853f',
+		pink: 'ffc0cb',
+		plum: 'dda0dd',
+		powderblue: 'b0e0e6',
+		rosybrown: 'bc8f8f',
+		royalblue: '4169e1',
+		saddlebrown: '8b4513',
+		salmon: 'fa8072',
+		sandybrown: 'f4a460',
+		seagreen: '2e8b57',
+		seashell: 'fff5ee',
+		sienna: 'a0522d',
+		skyblue: '87ceeb',
+		slateblue: '6a5acd',
+		slategray: '708090',
+		slategrey: '708090',
+		snow: 'fffafa',
+		springgreen: '00ff7f',
+		steelblue: '4682b4',
+		tan: 'd2b48c',
+		thistle: 'd8bfd8',
+		tomato: 'ff6347',
+		turquoise: '40e0d0',
+		violet: 'ee82ee',
+		wheat: 'f5deb3',
+		whitesmoke: 'f5f5f5',
+		yellowgreen: '9acd32',
+		rebeccapurple: '663399'
 	};
+	var hexKeywords = (function(source) {
+		var key, result = {};
+		for (key in source) {
+			result[source[key]] = key;
+		}
+		return result;
+	})(keywords);
 
 	var sourceToken = {
 		number: /(?:[-+]?(?:\d*\.)?\d+(?:e[-+]?\d+)?)/,
@@ -169,8 +176,8 @@
 		rgbapercent: /rgba\(\s*(percent)[,\s]+(percent)[,\s]+(percent)[,\s]+(unit)\)/,
 		hsl: /hsl\(\s*(hue)[,\s]+(percent)[,\s]+(percent)\s*\)/,
 		hsla: /hsla\(\s*(hue)[,\s]+(percent)[,\s]+(percent)[,\s]+(unit)\s*\)/,
-		hsv: /hsl\(\s*(hue)[,\s]+(percent)[,\s]+(percent)\s*\)/,
-		hsva: /hsla\(\s*(hue)[,\s]+(percent)[,\s]+(percent)[,\s]+(unit)\s*\)/,
+		hsv: /hsv\(\s*(hue)[,\s]+(percent)[,\s]+(percent)\s*\)/,
+		hsva: /hsva\(\s*(hue)[,\s]+(percent)[,\s]+(percent)[,\s]+(unit)\s*\)/,
 		hex1: /[0-9a-f]/,
 		hex2: /[0-9a-f]{2}/,
 		hex3: /#(hex1)(hex1)(hex1)/,
@@ -210,11 +217,12 @@
 	}
 
 	function isMatch (val, name) {
+		if (typeof val === 'undefined' || val === null) return null;
 		return token[name].exec(val);
 	}
 
 	function parseRgbNumber (val) {
-		return Math.min(255, Math.max(0, Math.floor(Number(val))));
+		return Math.min(255, Math.max(0, Number(val)));
 	}
 
 	function parsePercent (val) {
@@ -250,7 +258,7 @@
 	}
 
 	function toHexString (val) {
-		var s = parseInt(val, 16).toString();
+		var s = Math.round(val).toString(16);
 		return s.length < 2 ? '0' + s : s;
 	}
 
@@ -333,21 +341,29 @@
 		return { r: r, g: g, b: b };
 	}
 
-	function hsvToRgb(h, s, l) {
+	function hsvToRgb(h, s, v) {
+		h %= 360;
+		if (h < 0) h += 360;
 		s /= 100;
-		l /= 100;
-		var hi = Math.abs(h / 60) % 6;
+		v /= 100;
+		var hi = Math.floor(h / 60) % 6;
 		var f = h / 60 - hi;
 		var p = v * (1 - s);
 		var q = v * (1 - f * s);
 		var t = v * (1 - (1 - f) * s);
+		var r, g, b;
 		switch (hi) {
-			case 0: return { r: v, g: t, b: p };
-			case 1: return { r: q, g: v, b: p };
-			case 2: return { r: p, g: v, b: t };
-			case 3: return { r: p, g: q, b: v };
-			case 4: return { r: t, g: p, b: v };
-			case 5: return { r: v, g: p, b: q };
+			case 0: r = v; g = t; b = p; break;
+			case 1: r = q; g = v; b = p; break;
+			case 2: r = p; g = v; b = t; break;
+			case 3: r = p; g = q; b = v; break;
+			case 4: r = t; g = p; b = v; break;
+			case 5: r = v; g = p; b = q; break;
+		}
+		return {
+			r: r * 255,
+			g: g * 255,
+			b: b * 255,
 		}
 	}
 
@@ -359,92 +375,119 @@
 			return new Color(color, options);
 		}
 
+		var percentage = false;
+		var val;
+		var type;
+
 		if (typeof color === 'string') {
 			var match;
-			if (color in keywords) {
+			if (color === 'transparent') {
+				type = 'keyword';
+				color = { r: 0, g: 0, b: 0, a: 0 };
+			} else if (color in keywords) {
+				type = 'keyword';
 				color = keywords[color];
 				color = { r: color.slice(0, 1), g: color.slice(2, 3), b: color.slice(4, 5) };
-			} else if (match = isMatch(color, 'rgb') || isMatch(color, 'rgbpercent')) {
+			} else if (match = isMatch(color, 'rgb')) {
+				type = 'rgb';
 				color = { r: match[1], g: match[2], b: match[3] };
-			} else if (match = isMatch(color, 'rgba') || isMatch(color, 'rgbapercent')) {
+			} else if (match = isMatch(color, 'rgbpercent')) {
+				type = 'rgb';
+				percentage = true;
+				color = { r: match[1], g: match[2], b: match[3] };
+			} else if (match = isMatch(color, 'rgba')) {
+				type = 'rgb';
+				color = { r: match[1], g: match[2], b: match[3], a: match[4] };
+			} else if (match = isMatch(color, 'rgbapercent')) {
+				type = 'rgb';
+				percentage = true;
 				color = { r: match[1], g: match[2], b: match[3], a: match[4] };
 			} else if (match = isMatch(color, 'hex3')) {
-				color = { r: parseHex(match[1] + match[1]), g: parseHex(match[2] + match[2]), b: parseHex(match[3] + match[3]) };
+				type = 'hex';
+				color = { r: match[1] + match[1], g: match[2] + match[2], b: match[3] + match[3] };
 			} else if (match = isMatch(color, 'hex6')) {
-				color = { r: parseHex(match[1]), g: parseHex(match[2]), b: parseHex(match[3]) };
+				type = 'hex';
+				color = { r: match[1], g: match[2], b: match[3] };
 			} else if (match = isMatch(color, 'hex4')) {
-				color = { r: parseHex(match[1] + match[1]), g: parseHex(match[2] + match[2]), b: parseHex(match[3] + match[3]), a: parseHex(match[4] + match[4]) / 255 };
+				type = 'hex';
+				color = { r: match[1] + match[1], g: match[2] + match[2], b: match[3] + match[3], a: match[4] + match[4] };
 			} else if (match = isMatch(color, 'hex8')) {
-				color = { r: parseHex(match[1]), g: parseHex(match[2]), b: parseHex(match[3]), a: parseHex(match[4]) / 255 };
+				type = 'hex';
+				color = { r: match[1], g: match[2], b: match[3], a: match[4] };
 			} else if (match = isMatch(color, 'hsl')) {
+				type = 'hsl';
 				color = { h: match[1], s: match[2], l: match[3] };
 			} else if (match = isMatch(color, 'hsla')) {
+				type = 'hsl';
 				color = { h: match[1], s: match[2], l: match[3], a: match[4] };
 			} else if (match = isMatch(color, 'hsv')) {
+				type = 'hsv';
 				color = { h: match[1], s: match[2], v: match[3] };
 			} else if (match = isMatch(color, 'hsva')) {
+				type = 'hsv';
 				color = { h: match[1], s: match[2], v: match[3], a: match[4] };
 			}
 		}
 
-		var val = { r: 0, g: 0, b: 0, a: 1 };
-		var type = 'rgb';
-		var percentage = false;
-		if (typeof color === 'object') {
+		if (typeof type === 'undefined' && typeof color === 'object') {
 			if (isMatch(color.r, 'number') && isMatch(color.g, 'number') && isMatch(color.b, 'number')) {
 				type = 'rgb';
-				val = {
-					r: parseRgbNumber(color.r),
-					g: parseRgbNumber(color.g),
-					b: parseRgbNumber(color.b)
-				};
-				// if (val.r <= 1 && val.g <= 1 && val.b <= 1) {
-				// 	val.r *= 100;
-				// 	val.g *= 100;
-				// 	val.b *= 100;
-				// 	percentage = true;
-				// }
 			} else if (isMatch(color.r, 'percent') && isMatch(color.g, 'percent') && isMatch(color.b, 'percent')) {
 				type = 'rgb';
+				percentage = true;
+			} else if (isMatch(color.h, 'hue') && isMatch(color.s, 'unit') && isMatch(color.l, 'unit')) {
+				type = 'hsl';
+			} else if (isMatch(color.h, 'hue') && isMatch(color.s, 'unit') && isMatch(color.v, 'unit')) {
+				type = 'hsv';
+			}
+		}
+
+		if (type === 'hex') {
+			val = {
+				r: parseHex(color.r),
+				g: parseHex(color.g),
+				b: parseHex(color.b)
+			}
+			if (color.a) {
+				color.a = parseHex(color.a) / 255;
+			}
+		} else if (type === 'rgb') {
+			if (percentage === true) {
 				val = {
 					r: parsePercent(color.r),
 					g: parsePercent(color.g),
 					b: parsePercent(color.b)
 				};
-				percentage = true;
-			} else if (isMatch(color.h, 'hue') && isMatch(color.s, 'unit') && isMatch(color.l, 'unit')) {
-				type = 'hsl';
+			} else {
 				val = {
-					h: parseHue(color.h),
-					s: parseUnit(color.s),
-					l: parseUnit(color.l)
+					r: parseRgbNumber(color.r),
+					g: parseRgbNumber(color.g),
+					b: parseRgbNumber(color.b)
 				};
-				val = hslToRgb(val.h, val.s, val.l);
-				// if (val.s <= 1 && isMatch(color.s, 'number')) {
-				// 	val.s *= 100;
-				// }
-				// if (val.l <= 1 && isMatch(color.l, 'number')) {
-				// 	val.l *= 100;
-				// }
-			} else if (isMatch(color.h, 'hue') && isMatch(color.s, 'unit') && isMatch(color.v, 'unit')) {
-				type = 'hsv';
-				val = {
-					h: parseHue(color.h),
-					s: parseUnit(color.s),
-					v: parseUnit(color.v)
-				};
-				val = hslToRgb(val.h, val.s, val.v);
-				// if (val.s <= 1 && isMatch(color.s, 'number')) {
-				// 	val.s *= 100;
-				// }
-				// if (val.v <= 1 && isMatch(color.v, 'number')) {
-				// 	val.v *= 100;
-				// }
 			}
+		} else if (type === 'hsl') {
+			val = {
+				h: parseHue(color.h),
+				s: parseUnit(color.s),
+				l: parseUnit(color.l)
+			};
+			val = hslToRgb(val.h, val.s, val.l);
+		} else if (type === 'hsv') {
+			val = {
+				h: parseHue(color.h),
+				s: parseUnit(color.s),
+				v: parseUnit(color.v)
+			};
+			val = hsvToRgb(val.h, val.s, val.v);
+		}
+		if (typeof val === 'undefined') {
+			val = { r: 0, g: 0, b: 0, a: 1 };
+		} else {
 			val.a = isMatch(color.a, 'unit') ? parseAlpha(color.a) : 1;
 		}
-		this._type = type;
+
 		this._val = val;
+		this._type = type || 'hex';
 		this._percentage = percentage;
 	}
 
@@ -469,6 +512,13 @@
 			var type = oType || this._type;
 			var val = this._val;
 			var percent = this._percentage;
+			if (type === 'keyword') {
+				var hex = toHexString(val.r) + toHexString(val.g) + toHexString(val.b);
+				if (val !== 1 || !(hex in hexKeywords)) {
+					type = 'hex';
+				}
+				return  hexKeywords[hex];
+			}
 			if (type === 'rgb') {
 				if (percent) {
 					return val.a === 1 ?
@@ -476,8 +526,8 @@
 						'rgba(' + toFixed(val.r, decimalPoint) + '%,' + toFixed(val.g, decimalPoint) + '%,' + toFixed(val.b, decimalPoint) + '%,' + toFixed(val.a, decimalPoint) + ')';
 				} else {
 					return val.a === 1 ?
-					'rgb(' + val.r + ',' + val.g + ',' + val.b + ')' :
-					'rgba(' + val.r + ',' + val.g + ',' + val.b + ',' + toFixed(val.a, decimalPoint) + ')';
+					'rgb(' + Math.round(val.r) + ',' + Math.round(val.g) + ',' + Math.round(val.b) + ')' :
+					'rgba(' + Math.round(val.r) + ',' + Math.round(val.g) + ',' + Math.round(val.b) + ',' + toFixed(val.a, decimalPoint) + ')';
 				}
 			} else if (type === 'hsl') {
 				var hsl = rgbToHsl(val.r, val.g, val.b);
